9f0ab1f210add6fc4661f9e447ff4f1dca9cf7e0
mishinkinakatya <mishinkinakatya@gmail.com>
2020-02-08 18:16:04 +0500

    добавила изменение координат метки

--- js/main.js
+++ js/main.js
@@ -3,7 +3,10 @@
 // Количество объявлений
 var ADVERTISEMENT_COUNT = 8;
 
-/* массивы с данными для объявлений */
+// координаты метки по У
+var pinMinY = 130;
+var pinMaxY = 630;
+
 // Массив с вариантами типа жилья
 var accomodationTypes = ['palace', 'flat', 'house', 'bungalo'];
 
@@ -19,16 +22,10 @@ var accomodationFeatures = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator
 // Массив с фотографиями жилья
 var accomodationPhotos = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
 
-/* */
-
-/* общие функции*/
-
-// Функция для генерации случайных чисел в зависимости от длины массива
-var generateRandomNumbersForArray = function (arrayLength) {
-  return Math.floor(Math.random() * Math.floor(arrayLength - 1));
+// Функция для генерации случайных чисел
+var generateRandomNumbers = function (min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
 };
-
-/* */
 
 // Выбираем карту объявлений и пеерключаем ее в активное состояние
 var accomodationMap = document.querySelector('.map').classList.remove('map--faded');
@@ -39,45 +36,55 @@ var newPin = document.querySelector('#pin').content.querySelector('.map__pin');
 // Метки объявлений в разметке
 var advertisementPins = document.querySelector('.map__pins');
 
+// центр метки по горизонтали
+var widthPinHalf = newPin.offsetWidth / 2;
+
+// центр метки по вертикали
+var heightPin = newPin.offsetHeight;
+
+// координаты метки по Х
+var pinMinX = widthPinHalf;
+var pinMaxX = advertisementPins.offsetWidth - widthPinHalf;
+
 // Массив с похожими объявлениями
 var similarAdvertisements = [];
 
 // Функция для создания массива объектов - похожих объявлений неподалеку из 8 сгенерированных JS объектов.
-var generateDataForAdvertisements = function (count) {
+var generateAdvertisements = function (count) {
   for (var i = 0; i < count; i++) {
     similarAdvertisements.push({
       author: {
-        avatar: 'img/avatars/user0' + Math.floor(Math.random() * (8 - 1) + 1) + '.png',
+        avatar: 'img/avatars/user0' + (i + 1) + '.png',
       },
       offer: {
         title: 'Заголовок предложения',
         address: '600, 350',
         price: 1000,
-        type: accomodationTypes[generateRandomNumbersForArray(accomodationTypes)],
+        type: accomodationTypes[generateRandomNumbers(0, accomodationTypes.length)],
         rooms: 2,
         guests: 4,
-        checkin: checkinTimes[generateRandomNumbersForArray(checkinTimes)],
-        checkout: checkoutTimes[generateRandomNumbersForArray(checkoutTimes)],
-        features: accomodationFeatures[generateRandomNumbersForArray(accomodationFeatures)],
+        checkin: checkinTimes[generateRandomNumbers(0, checkinTimes.length)],
+        checkout: checkoutTimes[generateRandomNumbers(0, checkoutTimes.length)],
+        features: accomodationFeatures[generateRandomNumbers(0, accomodationFeatures.length)],
         description: 'Строка с описанием жилья',
-        photos: accomodationPhotos[generateRandomNumbersForArray(accomodationPhotos)],
+        photos: accomodationPhotos[generateRandomNumbers(0, accomodationPhotos.length)],
       },
       location: {
-        x: Math.floor(Math.random() * (630 - 130) + 130),
-        y: Math.floor(Math.random() * (630 - 130) + 130),
+        x: generateRandomNumbers(pinMinX, pinMaxX),
+        y: generateRandomNumbers(pinMinY, pinMaxY),
       }
     });
   }
   return similarAdvertisements;
 };
 
-// Создаем массив с похожими объявлениями
-var advertisements = generateDataForAdvertisements(ADVERTISEMENT_COUNT);
+// Заполняем массив похожими объявлениями
+var advertisements = generateAdvertisements(ADVERTISEMENT_COUNT);
 
 // функция создания DOM-элемента для метки
 var renderPin = function (pin) {
   var pinItem = newPin.cloneNode(true);
-  pinItem.setAttribute('style', 'left: ' + 150 + ' px; top: ' + 150 + ' px');
+  pinItem.setAttribute('style', 'left: ' + (pin.location.x - widthPinHalf) + 'px; top: ' + (pin.location.y - heightPin) + 'px');
   pinItem.querySelector('img').setAttribute('src', pin.author.avatar);
   pinItem.querySelector('img').setAttribute('alt', 'Заголовок объявления');
   return pinItem;
@@ -89,5 +96,5 @@ var fragment = document.createDocumentFragment();
 for (var j = 0; j < advertisements.length; j++) {
   fragment.appendChild(renderPin(advertisements[j]));
 }
-// добавление содержимого fragment в блок с метками
+
 advertisementPins.appendChild(fragment);
