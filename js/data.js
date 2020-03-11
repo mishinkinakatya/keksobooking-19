3a9327419c836fb1e60f36770e59e45e0b7df4ba
mishinkinakatya <mishinkinakatya@gmail.com>
2020-03-11 20:39:42 +0500

    удалила лишнее

--- js/data.js
+++ js/data.js
@@ -1,18 +1,6 @@
 'use strict';
 (function () {
   window.data = {
-    accommodationParameters: {
-      types: ['palace', 'flat', 'house', 'bungalo'],
-      checkinTimes: ['12:00', '13:00', '14:00'],
-      checkoutTimes: ['12:00', '13:00', '14:00'],
-      features: ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'],
-      photos: ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'],
-    },
-
-    generateRandomNumbers: function (min, max) {
-      return Math.floor(Math.random() * (max - min) + min);
-    },
-
     map: document.querySelector('.map'),
     mapPins: document.querySelector('.map__pins'),
     mainPin: document.querySelector('.map__pin--main'),
