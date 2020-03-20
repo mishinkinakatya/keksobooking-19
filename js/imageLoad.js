b131780016ad850848c156750773eb827edad70e
mishinkinakatya <mishinkinakatya@gmail.com>
2020-03-20 16:14:34 +0500

    рефакторинг загрузки и удаления фотографий

--- js/imageLoad.js
+++ js/imageLoad.js
@@ -5,14 +5,15 @@
   var PHOTO_SIZE = 70;
 
   var avatarChooser = document.querySelector('.ad-form__field input[type=file]');
-  var avatarPreview = document.querySelector('.ad-form-header__preview img');
-  var avatarPreviewSrc = avatarPreview.src;
+  var avatarPreview = document.querySelector('.ad-form-header__preview');
+  var avatarPreviewImg = avatarPreview.querySelector('img');
+  var avatarPreviewSrc = avatarPreviewImg.src;
   var photoChooser = document.querySelector('.ad-form__upload input[type=file]');
   var photoPreview = document.querySelector('.ad-form__photo');
-  var photoPreviewImg = document.createElement('img');
+  var photoPreviewImg;
 
-  avatarChooser.addEventListener('change', function () {
-    var file = avatarChooser.files[0];
+  var loadImage = function (chooser, preview) {
+    var file = chooser.files[0];
     var fileName = file.name.toLowerCase();
 
     var matches = FILE_TYPES.some(function (it) {
@@ -23,41 +24,38 @@
       var reader = new FileReader();
 
       reader.addEventListener('load', function () {
-        avatarPreview.src = reader.result;
+        var previewImg = preview.querySelector('img');
+        if (previewImg) {
+          previewImg.src = reader.result;
+        } else {
+          previewImg = document.createElement('img');
+          previewImg.src = reader.result;
+          previewImg.alt = 'Фотография жилья';
+          previewImg.width = PHOTO_SIZE;
+          previewImg.height = PHOTO_SIZE;
+          preview.appendChild(previewImg);
+
+          photoPreviewImg = photoPreview.querySelector('img');
+        }
       });
 
       reader.readAsDataURL(file);
     }
+  };
+
+  avatarChooser.addEventListener('change', function () {
+    loadImage(avatarChooser, avatarPreview);
   });
 
   photoChooser.addEventListener('change', function () {
-    var file = photoChooser.files[0];
-    var fileName = file.name.toLowerCase();
-
-    var matches = FILE_TYPES.some(function (it) {
-      return fileName.endsWith(it);
-    });
-
-    if (matches) {
-      var reader = new FileReader();
-
-      reader.addEventListener('load', function () {
-        photoPreviewImg.src = reader.result;
-        photoPreviewImg.alt = 'Фотография жилья';
-        photoPreviewImg.width = PHOTO_SIZE;
-        photoPreviewImg.height = PHOTO_SIZE;
-        photoPreview.appendChild(photoPreviewImg);
-      });
-
-      reader.readAsDataURL(file);
-    }
+    loadImage(photoChooser, photoPreview);
   });
 
   window.resetImage = function () {
     if (photoPreviewImg) {
       photoPreviewImg.remove();
     }
-    avatarPreview.src = avatarPreviewSrc;
+    avatarPreviewImg.src = avatarPreviewSrc;
   };
 
 })();
